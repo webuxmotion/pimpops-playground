af97f0ecaced31288e1a542b9e44eb03ed0aaa1e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-13 12:55:18 +0300

    Finish mega component desktop

--- site/View/themes/default/components/mega/mega.php
+++ site/View/themes/default/components/mega/mega.php
@@ -1,5 +1,19 @@
 <div class="mega">
-  <div class="mega__button mega__button_shape_rect" 
-    style="background-image: url('<?=assets()?>/images/html5-starter-rect.svg');">
+  <div class="g-container mega__container">
+    <div class="mega__cell"> 
+      <div class="mega__button mega__button_shape_circle" 
+        style="background-image: url('<?=assets()?>/images/tree-icon.svg');">
+      </div>
+    </div>
+    <div class="mega__cell"> 
+      <div class="mega__button mega__button_shape_rect" 
+        style="background-image: url('<?=assets()?>/images/html5-starter-rect.svg');">
+      </div>
+    </div>
+    <div class="mega__cell"> 
+      <div class="mega__button mega__button_shape_circle" 
+        style="background-image: url('<?=assets()?>/images/cards-icon.svg');">
+      </div>
+    </div>
   </div>
 </div>
