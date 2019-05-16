e1645bbae492c7734a2cc13d97e82c1d134cf630
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-16 19:33:51 +0300

    Add php-di-container model

--- site/View/themes/default/playgrounds/pimpops/php-di-container/data.php
+++ site/View/themes/default/playgrounds/pimpops/php-di-container/data.php
@@ -6,8 +6,17 @@ return [
   "modelPreview" => "/pimpops/php-di-container/images/model-preview.png",
   "githubLink" => "https://github.com/pimpops/php-di-container",
   "clickFunctions" => [
-    'index'  => 'canvas.showFile(\'index\');',
-    'app'    => 'canvas.showFile(\'app\');',
-    'styles' => 'canvas.showFile(\'styles\');'
+    'index-php'  => 'canvas.showFile(\'index-php\');',
+    'bootstrap-php'    => 'canvas.showFile(\'bootstrap-php\');',
+    'composer-json'    => 'canvas.showFile(\'composer-json\');',
+    'autoload-php'    => 'canvas.showFile(\'autoload-php\');',
+    'starter-php'    => 'canvas.showFile(\'starter-php\');',
+    'di-php'    => 'canvas.showFile(\'di-php\');',
+    'provider-list-php'    => 'canvas.showFile(\'provider-list-php\');',
+    'abstract-provider-php'    => 'canvas.showFile(\'abstract-provider-php\');',
+    'messenger-provider-php'    => 'canvas.showFile(\'messenger-provider-php\');',
+    'contact-book-provider-php'    => 'canvas.showFile(\'contact-book-provider-php\');',
+    'messenger-php'    => 'canvas.showFile(\'messenger-php\');',
+    'contact-book-php'    => 'canvas.showFile(\'contact-book-php\');',
   ]
 ];
