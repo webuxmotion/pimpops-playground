bccfede95f5d30b3fa159b775dfcbcf3c91b9d95
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-16 23:18:19 +0300

    Add cards view for php-di-container

--- site/View/themes/default/playgrounds/pimpops/php-di-container/cards.php
+++ site/View/themes/default/playgrounds/pimpops/php-di-container/cards.php
@@ -1,33 +1,119 @@
-<svg viewBox="0 0 389 296" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<style>#php-di-container-svg {
+width: 700px;
+}</style>
+<svg id="php-di-container-svg" viewBox="0 0 649 400" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 52.6 (67491) - http://www.bohemiancoding.com/sketch -->
+    <title>Group</title>
+    <desc>Created with Sketch.</desc>
+    <defs>
+        <rect id="path-1" x="518" y="0" width="131" height="132" rx="8"></rect>
+        <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="131" height="132" fill="white">
+            <use xlink:href="#path-1"></use>
+        </mask>
+    </defs>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="Group">
-            <g id="image">
-                <rect id="Rectangle" fill="#FFD19B" x="207" y="143" width="182" height="153" rx="8"></rect>
-                <text id="index.html" font-family="Arial-BoldMT, Arial" font-size="21" font-weight="bold" fill="#000000">
-                    <tspan x="220" y="172">index.html</tspan>
+            <g id="index.php" transform="translate(526.000000, 267.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#CFC3D0" x="0" y="0" width="115" height="102" rx="8"></rect>
+                <text font-family="Arial-BoldMT, Arial" font-size="16" font-weight="bold" fill="#736A74">
+                    <tspan x="20" y="55">index.php</tspan>
                 </text>
-                <rect id="Rectangle-Copy" fill="#FFD19B" x="0" y="85" width="138" height="77" rx="8"></rect>
-                <text id="app.js" font-family="Arial-BoldMT, Arial" font-size="21" font-weight="bold" fill="#000000">
-                    <tspan x="8" y="110">app.js</tspan>
-                </text>
-                <rect id="Rectangle-Copy-2" fill="#FFD19B" x="165" y="0" width="138" height="77" rx="8"></rect>
-                <text id="styles.css" font-family="Arial-BoldMT, Arial" font-size="21" font-weight="bold" fill="#000000">
-                    <tspan x="174" y="24">styles.css</tspan>
-                </text>
-                <path d="M178.94606,77 L178.94606,180.344407 C178.892722,188.044853 182.742945,191.895076 190.496729,191.895076 C198.250513,191.895076 203.751603,191.895076 207,191.895076" id="Path" stroke="#000000"></path>
-                <path d="M112.285574,162 L112.285574,253.25442 C112.652003,262.009111 117.029348,266.386456 125.417609,266.386456 C133.80587,266.386456 161,266.386456 207,266.386456" id="Path-2" stroke="#000000"></path>
-                <polyline id="Path-3" stroke="#000000" points="200 183 206.861495 191.771243 200 200.483519"></polyline>
-                <polyline id="Path-3-Copy" stroke="#000000" points="200 258 206.861495 266.771243 200 275.483519"></polyline>
             </g>
-<style>
-.rect-item:hover {
-  cursor: pointer;
-  fill-opacity: 0.3 !important;
-}
-</style>
-            <rect id="app.js" onclick="<?=$clickFunctions['app']?>" fill-opacity="0" class="rect-item" fill="#FFFFFF" x="0" y="85" width="138" height="77" rx="8"></rect>
-            <rect id="styles.css" onclick="<?=$clickFunctions['styles']?>" fill-opacity="0" class="rect-item" fill="#FFFFFF" x="165" y="0" width="138" height="77" rx="8"></rect>
-            <rect id="index.html" onclick="<?=$clickFunctions['index']?>" fill-opacity="0" class="rect-item" fill="#FFFFFF" x="207" y="143" width="182" height="153" rx="8"></rect>
+            <rect id="Rectangle" fill-opacity="0.577983469" fill="#CFC3D0" x="526" y="146" width="115" height="85" rx="8"></rect>
+            <text id="composer.json" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#736A74">
+                <tspan x="540" y="194">composer.json</tspan>
+            </text>
+            <rect id="Rectangle" fill-opacity="0.577983469" fill="#E0E0E0" x="533" y="20" width="102" height="85" rx="8"></rect>
+            <text id="autoload.php" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#A7A7A7">
+                <tspan x="546" y="66">autoload.php</tspan>
+            </text>
+            <rect id="Rectangle" fill-opacity="0.577983469" fill="#CFC3D0" x="343" y="267" width="138" height="102" rx="8"></rect>
+            <text id="bootstrap.php" font-family="Arial-BoldMT, Arial" font-size="16" font-weight="bold" fill="#736A74">
+                <tspan x="357" y="322">bootstrap.php</tspan>
+            </text>
+            <g id="starter.php" transform="translate(343.000000, 140.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="138" height="102" rx="8"></rect>
+                <text id="Starter.php" font-family="Arial-BoldMT, Arial" font-size="16" font-weight="bold" fill="#836484">
+                    <tspan x="29" y="55">Starter.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-2" transform="translate(187.000000, 322.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="117" height="50" rx="8"></rect>
+                <text id="providerList.php" font-family="Arial-BoldMT, Arial" font-size="12" font-weight="bold" fill="#836484">
+                    <tspan x="13" y="30">providerList.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-3" transform="translate(27.000000, 321.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="143" height="50" rx="8"></rect>
+                <text id="AbstractProvider.php" font-family="Arial-BoldMT, Arial" font-size="12" font-weight="bold" fill="#836484">
+                    <tspan x="11" y="30">AbstractProvider.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-4" transform="translate(38.000000, 233.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="110" height="68" rx="8"></rect>
+                <text id="Provider.php" font-family="Arial-BoldMT, Arial" font-size="15" font-weight="bold" fill="#836484">
+                    <tspan x="10" y="41">Provider.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-6" transform="translate(38.000000, 45.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="110" height="105" rx="8"></rect>
+                <text id="Messenger.php" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#836484">
+                    <tspan x="10" y="58">Messenger.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-7" transform="translate(183.000000, 46.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="110" height="105" rx="8"></rect>
+                <text id="ContactBook.php" font-family="Arial-BoldMT, Arial" font-size="12" font-weight="bold" fill="#836484">
+                    <tspan x="5" y="58">ContactBook.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy-5" transform="translate(184.000000, 233.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="110" height="68" rx="8"></rect>
+                <text id="Provider.php" font-family="Arial-BoldMT, Arial" font-size="15" font-weight="bold" fill="#836484">
+                    <tspan x="10" y="41">Provider.php</tspan>
+                </text>
+            </g>
+            <g id="starter.php-copy" transform="translate(343.000000, 14.000000)">
+                <rect id="Rectangle" fill-opacity="0.577983469" fill="#D19CD4" x="0" y="0" width="138" height="102" rx="8"></rect>
+                <text id="DI.php" font-family="Arial-BoldMT, Arial" font-size="16" font-weight="bold" fill="#836484">
+                    <tspan x="44" y="55">DI.php</tspan>
+                </text>
+            </g>
+            <rect id="core" stroke="#DBDBDB" stroke-width="2" x="1" y="1" width="506" height="398" rx="8"></rect>
+            <rect id="Rectangle" stroke="#DBDBDB" stroke-width="2" x="15" y="15" width="301" height="166" rx="8"></rect>
+            <rect id="Rectangle-Copy" stroke="#DBDBDB" stroke-width="2" x="15" y="206" width="301" height="178" rx="8"></rect>
+            <rect id="Rectangle-Copy-2" stroke="#DBDBDB" stroke-width="2" x="28" y="221" width="130" height="91" rx="8"></rect>
+            <rect id="Rectangle-Copy-4" stroke="#DBDBDB" stroke-width="2" x="28" y="33" width="129" height="130" rx="8"></rect>
+            <rect id="Rectangle-Copy-5" stroke="#DBDBDB" stroke-width="2" x="173" y="34" width="129" height="130" rx="8"></rect>
+            <use id="Rectangle-Copy-6" stroke="#DBDBDB" mask="url(#mask-2)" stroke-width="4" stroke-dasharray="6" xlink:href="#path-1"></use>
+            <rect id="Rectangle-Copy-3" stroke="#DBDBDB" stroke-width="2" x="173" y="222" width="130" height="91" rx="8"></rect>
+            <text id="core" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="18" y="398">core</tspan>
+            </text>
+            <text id="Worker" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="27" y="179">Worker</tspan>
+            </text>
+            <text id="Messenger" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="37" y="162">Messenger</tspan>
+            </text>
+            <text id="ContactBook" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="182" y="163">ContactBook</tspan>
+            </text>
+            <text id="Provider" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="27" y="383">Provider</tspan>
+            </text>
+            <text id="Messenger" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="39" y="311">Messenger</tspan>
+            </text>
+            <text id="ContactBook" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="187" y="312">ContactBook</tspan>
+            </text>
+            <text id="Vendor" font-family="Arial-BoldMT, Arial" font-size="13" font-weight="bold" fill="#DBDBDB">
+                <tspan x="529" y="129">Vendor</tspan>
+            </text>
+            <g id="index.php-copy" transform="translate(526.000000, 146.000000)"></g>
+            <g id="index.php-copy-2" transform="translate(533.000000, 20.000000)"></g>
+            <g id="bootstrap" transform="translate(343.000000, 267.000000)"></g>
         </g>
     </g>
 </svg>
--- site/View/themes/default/playgrounds/pimpops/php-di-container/tree.php
+++ site/View/themes/default/playgrounds/pimpops/php-di-container/tree.php
@@ -12,7 +12,169 @@
   <div class="tree__folder is-active">
     <?php
       $data['treeItem'] = [
-        'onClick' => $clickFunctions['index'],
+        'onClick' => 'treeItem.toggle(this);',
+        'icon' => 'folder-icon.svg',
+        'iconSecondary' => 'folder-opened-icon.svg',
+        'classes' => 'tree-item_type_folder is-active',
+        'text' => 'core'
+      ];
+      $ctx->theme->block('components/tree-item/tree-item', $data);
+    ?>  
+    <div class="tree__folder is-active">
+      <?php
+        $data['treeItem'] = [
+          'onClick' => 'treeItem.toggle(this);',
+          'icon' => 'folder-icon.svg',
+          'iconSecondary' => 'folder-opened-icon.svg',
+          'classes' => 'tree-item_type_folder',
+          'text' => 'Provider'
+        ];
+        $ctx->theme->block('components/tree-item/tree-item', $data);
+      ?>  
+      <div class="tree__folder">
+        <?php
+          $data['treeItem'] = [
+            'onClick' => 'treeItem.toggle(this);',
+            'icon' => 'folder-icon.svg',
+            'iconSecondary' => 'folder-opened-icon.svg',
+            'classes' => 'tree-item_type_folder',
+            'text' => 'Messenger'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+        <div class="tree__folder">
+          <?php
+            $data['treeItem'] = [
+              'onClick' => $clickFunctions['messenger-provider-php'],
+              'icon' => 'file-icon.svg',
+              'classes' => 'tree-item_type_file',
+              'text' => 'Provider.php'
+            ];
+            $ctx->theme->block('components/tree-item/tree-item', $data);
+          ?>  
+        </div>
+        <?php
+          $data['treeItem'] = [
+            'onClick' => 'treeItem.toggle(this);',
+            'icon' => 'folder-icon.svg',
+            'iconSecondary' => 'folder-opened-icon.svg',
+            'classes' => 'tree-item_type_folder',
+            'text' => 'ContactBook'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+        <div class="tree__folder">
+          <?php
+            $data['treeItem'] = [
+              'onClick' => $clickFunctions['contact-book-provider-php'],
+              'icon' => 'file-icon.svg',
+              'classes' => 'tree-item_type_file',
+              'text' => 'Provider.php'
+            ];
+            $ctx->theme->block('components/tree-item/tree-item', $data);
+          ?>  
+        </div>
+        <?php
+          $data['treeItem'] = [
+            'onClick' => $clickFunctions['abstract-provider-php'],
+            'icon' => 'file-icon.svg',
+            'classes' => 'tree-item_type_file',
+            'text' => 'AbstractProvider.php'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+        <?php
+          $data['treeItem'] = [
+            'onClick' => $clickFunctions['provider-list-php'],
+            'icon' => 'file-icon.svg',
+            'classes' => 'tree-item_type_file',
+            'text' => 'providerList.php'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+      </div>
+      <?php $data['treeItem'] = [
+          'onClick' => 'treeItem.toggle(this);',
+          'icon' => 'folder-icon.svg',
+          'iconSecondary' => 'folder-opened-icon.svg',
+          'classes' => 'tree-item_type_folder',
+          'text' => 'Worker'
+        ];
+        $ctx->theme->block('components/tree-item/tree-item', $data);
+      ?>  
+      <div class="tree__folder">
+        <?php $data['treeItem'] = [
+            'onClick' => 'treeItem.toggle(this);',
+            'icon' => 'folder-icon.svg',
+            'iconSecondary' => 'folder-opened-icon.svg',
+            'classes' => 'tree-item_type_folder',
+            'text' => 'Messenger'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+        <div class="tree__folder">
+          <?php
+            $data['treeItem'] = [
+              'onClick' => $clickFunctions['messenger-php'],
+              'icon' => 'file-icon.svg',
+              'classes' => 'tree-item_type_file',
+              'text' => 'Messenger.php'
+            ];
+            $ctx->theme->block('components/tree-item/tree-item', $data);
+          ?>  
+        </div>
+        <?php $data['treeItem'] = [
+            'onClick' => 'treeItem.toggle(this);',
+            'icon' => 'folder-icon.svg',
+            'iconSecondary' => 'folder-opened-icon.svg',
+            'classes' => 'tree-item_type_folder',
+            'text' => 'ContactBook'
+          ];
+          $ctx->theme->block('components/tree-item/tree-item', $data);
+        ?>  
+        <div class="tree__folder">
+          <?php
+            $data['treeItem'] = [
+              'onClick' => $clickFunctions['contact-book-php'],
+              'icon' => 'file-icon.svg',
+              'classes' => 'tree-item_type_file',
+              'text' => 'ContactBook.php'
+            ];
+            $ctx->theme->block('components/tree-item/tree-item', $data);
+          ?>  
+        </div>
+      </div>
+      <?php
+        $data['treeItem'] = [
+          'onClick' => $clickFunctions['bootstrap-php'],
+          'icon' => 'file-icon.svg',
+          'classes' => 'tree-item_type_file',
+          'text' => 'bootstrap.php'
+        ];
+        $ctx->theme->block('components/tree-item/tree-item', $data);
+      ?>  
+      <?php
+        $data['treeItem'] = [
+          'onClick' => $clickFunctions['di-php'],
+          'icon' => 'file-icon.svg',
+          'classes' => 'tree-item_type_file',
+          'text' => 'DI.php'
+        ];
+        $ctx->theme->block('components/tree-item/tree-item', $data);
+      ?>  
+      <?php
+        $data['treeItem'] = [
+          'onClick' => $clickFunctions['starter-php'],
+          'icon' => 'file-icon.svg',
+          'classes' => 'tree-item_type_file',
+          'text' => 'Starter.php'
+        ];
+        $ctx->theme->block('components/tree-item/tree-item', $data);
+      ?>  
+    </div>
+    <?php
+      $data['treeItem'] = [
+        'onClick' => $clickFunctions['index-php'],
         'icon' => 'file-icon.svg',
         'classes' => 'tree-item_type_file',
         'text' => 'index.php'
@@ -21,19 +183,10 @@
     ?>  
     <?php
       $data['treeItem'] = [
-        'onClick' => $clickFunctions['styles'],
+        'onClick' => $clickFunctions['composer-json'],
         'icon' => 'file-icon.svg',
         'classes' => 'tree-item_type_file',
-        'text' => 'styles.css'
-      ];
-      $ctx->theme->block('components/tree-item/tree-item', $data);
-    ?>  
-    <?php
-      $data['treeItem'] = [
-        'onClick' => $clickFunctions['app'],
-        'icon' => 'file-icon.svg',
-        'classes' => 'tree-item_type_file',
-        'text' => 'app.js'
+        'text' => 'composer.json'
       ];
       $ctx->theme->block('components/tree-item/tree-item', $data);
     ?>  
